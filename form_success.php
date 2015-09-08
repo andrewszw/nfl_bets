e42a05691a00b68cbae1bf20ed6c0c25db192f87
andrewszw <zacharyandrews93@gmail.com>
2015-09-08 17:13:32 -0400

    form now submits to enter teams

--- form_success.php
+++ form_success.php
@@ -1,5 +1,5 @@
 <?php include_once('includes/header.php'); ?>
-<?php include_once('includes/db.php'); ?>
+<?php include('includes/db.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,32 +12,29 @@
 <body>
 <?php
     // turn on error reporting
-    error_reporting(0);
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
 
-    // make sure submit button has been clicked
-    if($_POST['submit']) {
-
-        // get the form input
-        $name = trim($_POST['name']);
-        $week = trim($_POST['week']);
-        $one = strtolower($_POST['game1']);
-        $two = strtolower($_POST['game2']);
-        $three = strtolower($_POST['game3']);
-        $four = strtolower($_POST['game4']);
-        $five = strtolower($_POST['game5']);
-        $six = strtolower($_POST['game6']);
-        $seven = strtolower($_POST['game7']);
-        $eight = strtolower($_POST['game8']);
-        $nine = strtolower($_POST['game9']);
-        $ten = strtolower($_POST['game10']);
-        $eleven = strtolower($_POST['game11']);
-        $twelve = strtolower($_POST['game12']);
-        $thirteen = strtolower($_POST['game13']);
-        $fourteen = strtolower($_POST['game14']);
-        $fifteen = strtolower($_POST['game15']);
-        $sixteen = strtolower($_POST['game16']);
-        $current = date("Y-m-d H:i:s");
-    }
+    // get the form input
+    $name = trim($_POST['name']);
+    $week = trim($_POST['week_no']);
+    $one = strtolower($_POST['one']);
+    $two = strtolower($_POST['two']);
+    $three = strtolower($_POST['three']);
+    $four = strtolower($_POST['four']);
+    $five = strtolower($_POST['five']);
+    $six = strtolower($_POST['six']);
+    $seven = strtolower($_POST['seven']);
+    $eight = strtolower($_POST['eight']);
+    $nine = strtolower($_POST['nine']);
+    $ten = strtolower($_POST['ten']);
+    $eleven = strtolower($_POST['eleven']);
+    $twelve = strtolower($_POST['twelve']);
+    $thirteen = strtolower($_POST['thirteen']);
+    $fourteen = strtolower($_POST['fourteen']);
+    $fifteen = strtolower($_POST['fifteen']);
+    $sixteen = strtolower($_POST['sixteen']);
+    $current = date("Y-m-d H:i:s");
 
     // build the query
     $query ="INSERT INTO nflbets
@@ -63,7 +60,7 @@
              '$current')";
 
     // run the query
-    $result = mysqli_query($con, $query);
+    $result = $con->query($query);
 
     // test the query
     if(!$result) {
@@ -71,7 +68,7 @@
     }
 
     // close the connection
-    mysqli_close($con);
+    $con->close();
 ?>
 
     <div class="container-fluid">
--- js/form.js
+++ js/form.js
@@ -10,13 +10,15 @@ $(document).ready(function() {
             nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven',
                     'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
                     'fourteen', 'fifteen', 'sixteen'];
-
+            num_counter = 0;
+            
             for(i = 1; i < 17; i++) {  
                 var form_div = document.createElement('div');
                 form_div.className = "form-group";
-                form_div.innerHTML += "<label for='" + nums[i] + "'>" + data[i] + "</label>";
-                form_div.innerHTML += "<input type='text' class='form-control' name='" + nums[i] + "'>";
+                form_div.innerHTML += "<label for='" + nums[num_counter] + "'>" + data[i] + "</label>";
+                form_div.innerHTML += "<input type='text' class='form-control' name='" + nums[num_counter] + "'>";
                 div.appendChild(form_div);
+                num_counter++;
             }
           }
     })
